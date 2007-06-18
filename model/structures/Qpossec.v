412f8a39b66147f62c5ed9778fe170a69c010348
Russell O'Connor <r.oconnor@cs.ru.nl>
2007-06-18 14:51:50 +0000

    Adding missing lemmas about Qpos.
    git-archimport-id: r.oconnor@cs.ru.nl--Nijmegen/CoRN--roconnor-dev--3--patch-24

--- model/structures/Qpossec.v
+++ model/structures/Qpossec.v
@@ -36,8 +36,10 @@
 
 (** printing Qpos $\mathbb{Q}^{+}$ #Q<SUP>+</SUP># *)
 
+Require Export QArith.
 Require Import Qordfield.
 Require Import COrdFields2.
+Require Import Eqdep_dec.
 Require Import CornTac.
 
 Record Qpos : Set := QposMake 
@@ -93,6 +95,8 @@ discriminate.
 reflexivity.
 discriminate.
 Qed.
+
+Implicit Arguments QposAsmkQpos [a].
 
 Lemma QposAsQposMake : forall a b, (QposAsQ (QposMake a b)) = (Zpos a)#b.
 Proof.
@@ -199,6 +203,8 @@ exists (mkQpos H0).
 QposRing.
 Defined.
 
+Implicit Arguments Qpos_lt_plus [a b].
+
 Definition QposSum (l:list Qpos) : Q := fold_right
 (fun (x:Qpos) (y:Q) => x+y) (Zero:Q) l.
 
@@ -213,3 +219,34 @@ apply Qpos_prf.
 assumption.
 Qed.
 
+Lemma QposRed_prf : forall (a:Q), (0 < a) -> (0 < Qred a).
+Proof.
+intros a Ha.
+rewrite Qred_correct.
+assumption.
+Qed.
+
+Definition QposRed (a:Qpos) : Qpos := mkQpos (QposRed_prf a (Qpos_prf a)).
+
+Lemma QposRed_complete : forall p q : Qpos, p == q -> QposRed p = QposRed q.
+Proof.
+intros p q H.
+unfold QposRed.
+generalize (QposRed_prf p (Qpos_prf p)).
+generalize (QposRed_prf q (Qpos_prf q)).
+rewrite (Qred_complete p q H).
+unfold Qlt, Zlt.
+intros A B.
+assert (X:forall x y : comparison, x = y \/ x <> y).
+decide equality.
+rewrite (eq_proofs_unicity X A B).
+reflexivity.
+Qed.
+
+Lemma QposRed_correct : forall p, QposRed p == p.
+Proof.
+unfold QposRed.
+intros p.
+autorewrite with QposElim.
+apply Qred_correct.
+Qed.
