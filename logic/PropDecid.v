1ddbb92edde012ddf5441b749fa5a1d54a2253ea
Cezary Kaliszyk <cek@cs.ru.nl>
2008-01-30 15:22:44 +0000

    Prop Decidable
    git-archimport-id: cek@cs.ru.nl--software/CoRN--cek--3--patch-7

---
+++ logic/PropDecid.v
@@ -0,0 +1,26 @@
+(* Copyright  2008-2008
+ * Cezary Kaliszyk
+ *
+ * This work is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This work is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this work; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+Lemma imp_dec : (forall A B, ({A} + {~A}) -> ({B} + {~B}) -> ({A -> B} + {~(A -> B)})).
+intros a b A B .
+destruct A as [A | A].
+destruct B as [B | B].
+left; tauto.
+right; tauto.
+left; tauto.
+Qed.
