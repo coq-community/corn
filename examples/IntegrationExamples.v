a96d1030cc37326461e5fa8c378c95e5c3800888
Russell O'Connor <r.oconnor@cs.ru.nl>
2008-03-11 01:57:35 +0000

    Adding examples on how to compute with real numbers and plotting.
    git-archimport-id: r.oconnor@cs.ru.nl--Nijmegen/CoRN--roconnor-dev--3--patch-108

---
+++ examples/IntegrationExamples.v
@@ -0,0 +1,49 @@
+(*
+Copyright © 2006 Russell O’Connor
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this proof and associated documentation files (the "Proof"), to deal in
+the Proof without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Proof, and to permit persons to whom the Proof is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Proof.
+
+THE PROOF IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE PROOF OR THE USE OR OTHER DEALINGS IN THE PROOF.
+*)
+
+Require Import Integration.
+Require Import CRtrans.
+
+(* The answer function returns an approximation of r within 10^-n.
+Take the resulting integer and divide by 10^n to get the actual rational approximation.
+answer is useful for because it displays a familar list of digits rather
+than an unfamiliar fraction that approximate would return *)
+
+Definition answer (n:positive) (r:CR) : Z :=
+ let m := (iter_pos n _ (Pmult 10) 1%positive) in
+ let (a,b) := (approximate r (1#m)%Qpos)*m in
+ Zdiv a b.
+
+(* This file illustrates how to use the computational integration *)
+(* Please review RealFast.v for examples on how to compute with CR *)
+
+(* Integrate01 requires that we integrate uniformly continuous functions.
+   Therefore we cannot integerate (sin : CR -> CR), me must instead
+   integrate the UniformlyContinuousFunction (sin_uc : Q --> CR). *)
+Time Eval vm_compute in answer 3 (Integrate01 sin_uc).
+
+(* find the supremum of cos on [0,1] *)
+Time Eval vm_compute in answer 3 (ContinuousSup01 cos_uc).
+
+(* find the supremum of id on [0,1] *)
+Time Eval vm_compute in answer 3 (ContinuousSup01 Cunit).
+
+
---
+++ examples/PlotExamples.v
@@ -0,0 +1,66 @@
+(*
+Copyright © 2006 Russell O’Connor
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this proof and associated documentation files (the "Proof"), to deal in
+the Proof without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Proof, and to permit persons to whom the Proof is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Proof.
+
+THE PROOF IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE PROOF OR THE USE OR OTHER DEALINGS IN THE PROOF.
+*)
+
+Require Import Plot.
+Require Import CRtrans.
+
+(* `∗' is used for trival proofs that a some concrete number is less than another *)
+Definition star := refl_equal Lt.
+Notation "∗" := star.
+
+Open Local Scope Q_scope.
+
+Open Local Scope raster.
+
+(* This file illustrates how to plot funcitons *)
+(* PlotQ requires that we plot uniformly continuous functions.
+   Therefore we cannot plot (sin : CR -> CR), me must instead
+   plot the UniformlyContinuousFunction (sin_uc : Q --> CR). *)
+(* Here we plot sin on [-3,3] with range [-1,1] on a 36x12 raster *)
+Time Eval vm_compute in PlotQ (- (3)) 3 ∗ (- (1)) 1 ∗ sin_uc 36 12.
+
+(* Here we explore the proof that plots are correct *)
+Goal True.
+(* Plot_correct is a proof that the plot is correct.*)
+(* below we plot exp on [-6, 0] with range [0,1] *)
+(* (exp_bound_uc 0) is exp on ]-inf,0] which is one domain where it is uniformly continuous *)
+assert (X:=@Plot_correct (-(6)) 0 star 0 1 star
+ (exp_bound_uc 0)
+ 69 23 (refl_equal _) (refl_equal _)).
+(* No plot is seen.  It is hidden in the uncomputed
+  PlotQ (- (6)) 0 ∗ 0 1 ∗ (exp_bound_uc 0) 69 23 *)
+(* We use patern matchin to extract the parts of the statement we
+   wish to normalize *)
+match goal with
+ [X:ball ?e ?a (@ucFun _ _ _ (_⇱?b⇲_))|-_] => set (E:=e) in X; set (B:=b) in X
+end.
+(* E is the error; a bound on the distance between our plot and the actual function *)
+vm_compute in E.
+(* The error is 2508/38640 *)
+(* B is the plot *)
+Time vm_compute in B.
+(* The plot is a 69 by 23 raster. *)
+(* The plot and error can be reinserted into the statement if you wish *)
+unfold E, B in X.
+clear E B.
+(* end this example *)
+split.
+Qed.
