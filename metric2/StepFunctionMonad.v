66c2a7fe2f96ab17062d5941185a524867d53087
Bas Spitters <spitters@cs.ru.nl>
2008-06-02 20:10:01 +0000

    Trying definition of Join
    git-archimport-id: spitters@cs.ru.nl--Nijmegen/CoRN--integration--1--patch-5

--- metric2/StepFunctionMonad.v
+++ metric2/StepFunctionMonad.v
@@ -58,16 +58,89 @@ How to continue??*)
 
 (* Problem: what is the right encoding*)
 Definition StFJoin0 (X:Setoid):(StepFS (StepFS X)) -> (StepFS X).
-intros X m.
-apply (@StepFfold (StepFS X)).
-exact (@id (StepFS X)).
-exact (fun o l r => @glue X o (SplitL l o) (SplitR r o)).
-exact m.
+fix 2. intros X m. case m.
+exact (fun x=> x). 
+intros o m1 m2.
+exact (glue o (StFJoin0 X (Map (fun m => (@SplitL X m o)) m1))
+                      (StFJoin0 X (Map (fun m => (@SplitR X m o)) m2))).
 Defined.
 
 
-Axiom StFJoin_wd: forall X:Setoid, forall m m1:(StepFS (StepFS X)), m==m1 -> 
+Lemma StFJoin_wd: forall X:Setoid, forall m m1:(StepFS (StepFS X)), m==m1 -> 
   (StFJoin0 m)==(StFJoin0 m1).
+intros X m. 
+induction m.
+intros. unfold StFJoin0. simpl.
+induction m1 using StepF_ind.
+  assumption.
+ revert IHm1_1. revert IHm1_2. unfold StFJoin0. simpl. intros.
+ assert (H1:(constStepF (X:=StepFS X) x == m1_1)).
+  pose (s:=SplitL_resp_Xeq (constStepF (X:=StepFS X) x) 
+   (glue o m1_1 m1_2) o H). rewrite SplitLGlue  in s. rewrite s. auto with *.
+ assert (H2:(constStepF (X:=StepFS X) x == m1_2)).
+  pose (s1:=SplitR_resp_Xeq (constStepF (X:=StepFS X) x) 
+   (glue o m1_1 m1_2) o H). rewrite SplitRGlue  in s1. rewrite s1. auto with *.
+ symmetry. apply glue_StepF_eq.
+ rewrite (IHm1_1 H1). reflexivity.
+ rewrite (IHm1_2 H2). reflexivity.
+intros.
+ pose (s:=SplitL_resp_Xeq 
+   (glue o m1 m2) m0 o H). rewrite SplitLGlue in s.
+ unfold StFJoin0. simpl.
+ change (glue o
+  (SplitL
+     (StFJoin0 m1) o)
+  (SplitR
+     (StFJoin0 m2) o) ==
+   (StFJoin0 m0)).
+rewrite (IHm1 _ s).
+ pose (t:=SplitR_resp_Xeq 
+   (glue o m1 m2) m0 o H). rewrite SplitRGlue in t.
+rewrite (IHm2 _ t). 
+clear s t.
+clear H IHm1 IHm2.
+clear m1 m2.
+revert o.
+assert (forall m:StepFS (StepFS X), forall o : OpenUnit,
+glue o (SplitL (StFJoin0 (SplitL m o)) o)
+  (SplitR (StFJoin0 (SplitR m o)) o) == StFJoin0 m).
+
+
+
+induction m0.
+unfold StFJoin0. simpl.
+apply glueSplit.
+clear o.
+unfold StFJoin0. simpl.
+
+
+
+
+
+
+
+ m1.
+induction m using StepF_ind. induction m1 using StepF_ind.
+  intros. assumption.
+ intro. revert IHm1_1. revert IHm1_2. unfold StFJoin0. simpl. intros.
+ assert (H1:(constStepF (X:=StepFS X) x == m1_1)).
+  pose (s:=SplitL_resp_Xeq (constStepF (X:=StepFS X) x) 
+   (glue o m1_1 m1_2) o H). rewrite SplitLGlue  in s. rewrite s. auto with *.
+ assert (H2:(constStepF (X:=StepFS X) x == m1_2)).
+  pose (s1:=SplitR_resp_Xeq (constStepF (X:=StepFS X) x) 
+   (glue o m1_1 m1_2) o H). rewrite SplitRGlue  in s1. rewrite s1. auto with *.
+ symmetry. apply glue_StepF_eq.
+ rewrite (IHm1_1 H1). reflexivity.
+ rewrite (IHm1_2 H2). reflexivity.
+intro.
+unfold StFJoin0.
+simpl. change (glue o
+  (SplitL
+     (StFJoin0 m2) o)
+  (SplitR
+     (StFJoin0 m3) o) ==
+   (StFJoin0 m1)).
+Admitted.
 
 
 Definition StFJoin (X:Setoid):(StepFS (StepFS X)) --> (StepFS X).
@@ -153,8 +226,7 @@ pose (Map (fun x=> SplitR x o) g).
 apply (Map2 (glue o) s s0).
 Defined.
 
-Print Diag.
-
+(*
 Lemma JoinDiag:forall m, (StFJoin X m)==(StFJoin X (Diag m)).
 intros.
 induction m.
@@ -180,13 +252,33 @@ StFJoin0
 
 unfold StFJoin0.
 simpl.
-
+*)
 
 
 
 
 
 Lemma BindReturn(m:StepF X): (StFBind m (StFReturn X)) == m.
+intro m.
+unfold StFBind.
+induction m using StepF_ind.
+unfold SSplitL. simpl. auto with *.
+rewrite MapGlue.
+unfold SSplitL. simpl.
+rewrite IHm1. rewrite IHm2.
+
+
+map ret (glue o (const x1) (const x2)) is
+
+glue o (const (ret x1)) (const (ret x2))
+
+now if we apply gjoin to it we get.
+
+glue o (const x1) (const x2)
+
+which is what we started with.
+
+
 intro m.
 unfold StFBind.
 assert ((StFReturn X ^@> m) == (SSplitL m)).
