f5d393bb1c1f80498c6ea18ec87c24b44e5ca1ac
Bas Spitters <spitters@cs.ru.nl>
2008-06-03 14:16:06 +0000

    Proved the monad laws. Still have to do extensionality.
    git-archimport-id: spitters@cs.ru.nl--Nijmegen/CoRN--integration--1--patch-6

--- metric2/StepFunctionMonad.v
+++ metric2/StepFunctionMonad.v
@@ -39,119 +39,116 @@ exists (@constStepF X).
 auto with *.
 Defined.
 
-(*
-Definition StepFfoldS (X Y:Setoid): (X --> Y) -> (OpenUnit -> Y --> Y --> Y) 
-     -> (StepFS X)--> Y.
-intros X Y f g.
-exists (@StepFfold X Y f (fun o y=>g o y)).
-intros. revert H.
-induction x1 using StepF_ind.
- induction x2 using StepF_ind.
-  simpl. intro. apply f. assumption.
- simpl. intro.
- assert (t:(glue o (constStepF x) (constStepF x)) ==  glue o x2_1 x2_2).
- rewrite <-H. apply glue_StepF_eq; auto with *.
- clear H.
- pose (IHx2_2 (glue_injr _ _ _ _  _ t)). pose (IHx2_1 (glue_injl _ _ _ _  _ t)).
- set (SF:=(@StepFfold X Y f (fun o y=>g o y))).
-How to continue??*)
+Definition SplitLS0(X:Setoid):OpenUnit->(StepFS X)->(StepFS X):=
+(fun o x => SplitL x o).
 
-(* Problem: what is the right encoding*)
-Definition StFJoin0 (X:Setoid):(StepFS (StepFS X)) -> (StepFS X).
-fix 2. intros X m. case m.
-exact (fun x=> x). 
-intros o m1 m2.
-exact (glue o (StFJoin0 X (Map (fun m => (@SplitL X m o)) m1))
-                      (StFJoin0 X (Map (fun m => (@SplitR X m o)) m2))).
+Definition SplitLS(X:Setoid):OpenUnit->(StepFS X)-->(StepFS X).
+intros X o.
+exists (fun x => (SplitLS0 o x)).
+intros. rapply SplitL_wd;auto with *.
 Defined.
 
+Definition SplitRS0(X:Setoid):OpenUnit->(StepFS X)->(StepFS X):=
+(fun o x => SplitR x o).
 
+Definition SplitRS(X:Setoid):OpenUnit->(StepFS X)-->(StepFS X).
+intros X o.
+exists (fun x => (SplitRS0 o x)).
+intros. rapply SplitR_wd;auto with *.
+Defined.
+
+Definition StFBind00(X Y:Setoid) : 
+  (StepFS X) -> (X --> (StepFS Y)) -> (StepFS Y).
+intros X Y .
+fix 1. intro m. case m.
+intros x f.
+exact (f x).
+intros o m1 m2 f.
+exact (glue o (StFBind00 m1 (compose (SplitLS Y o) f))
+                     (StFBind00 m2 (compose (SplitRS Y o) f))).
+Defined.
+
+Axiom cheat:False.
+Definition StFBind1(X Y:Setoid) : 
+  (StepFS X) -> (X --> (StepFS Y)) --> (StepFS Y).
+intros X Y m.
+exists (fun f=> (@StFBind00 X Y m f)).
+induction m.
+ intros. simpl; auto with *. apply H.
+intros. simpl. apply glue_resp_StepF_eq.
+ apply IHm1. intro. simpl. unfold compose0. 
+ apply SplitL_wd; auto with *. apply H.
+apply IHm2. intro. simpl. unfold compose0. 
+apply SplitR_wd; auto with *. apply H.
+Defined.
+
+Definition StFBind(X Y:Setoid) : 
+  (StepFS X) --> (X --> (StepFS Y)) --> (StepFS Y).
+intros X Y.
+exists (fun m => (@StFBind1 X Y m)).
+intros x1.
+induction x1.  
+ induction x2. simpl. intro; auto with *.
+  intro f. apply f. auto with *. 
+ simpl. intro H.
+ (* Should be a Lemma *)
+ change (StepF_eq (StepFunction.constStepF x) (glue o x2_1 x2_2)) in H.
+ pose (s:=SplitL_resp_Xeq _ _ o H). rewrite SplitLGlue in s.
+ change (StepF_eq (constStepF x)  x2_1) in s.
+ pose (s1:=IHx2_1 s). 
+ intro f. 
+ pose (s1 (compose1 (SplitLS Y o) f)). rewrite <- s0.
+ simpl. unfold compose0. clear s1 s0 s IHx2_1. 
+ pose (s:=SplitR_resp_Xeq _ _ o H). rewrite SplitRGlue in s.
+ clear H. change (StepF_eq (constStepF x)  x2_2) in s.
+ pose (s1:=IHx2_2 s).  
+ pose (s1 (compose1 (SplitRS Y o) f)). rewrite <- s0.
+ simpl. unfold compose0. unfold SplitLS0, SplitRS0. symmetry. apply glueSplit.
+
+intro x2.
+change (st_eq (StepFS X) (glue o x1_1 x1_2) x2 ->
+st_eq ((X --> StepFS Y) --> StepFS Y)
+  (StFBind1 Y (glue o x1_1 x1_2)) (StFBind1 Y x2)).
+intro H.
+pose (s:=SplitL_resp_Xeq _ _ o H). rewrite SplitLGlue in s.
+pose (t:=SplitR_resp_Xeq _ _ o H). rewrite SplitRGlue in t.
+intro f. simpl.
+pose (IHx1_1 _ s (compose1 (SplitLS Y o) f)).
+rewrite s0. clear s0.
+pose (IHx1_2 _ t (compose1 (SplitRS Y o) f)).
+rewrite s0. clear s0 IHx1_1 IHx1_2. 
+change ((StFBind1 _ (glue o (SplitL x2 o) (SplitR x2 o)) f)  == StFBind00 x2 f).
+clear H s t x1_1 x1_2.
+
+rename x2 into y. revert o.
+induction y. simpl; auto with *. unfold compose0, SplitLS0, SplitRS0. apply glueSplit.
+intro p.
+change (StFBind1 Y
+  (glue p (SplitL (glue o y1 y2) p)
+     (SplitR (glue o y1 y2) p)) f ==
+StFBind00 (glue o y1 y2) f).
+
+
+
+Defined.
+
+(*
 Lemma StFJoin_wd: forall X:Setoid, forall m m1:(StepFS (StepFS X)), m==m1 -> 
   (StFJoin0 m)==(StFJoin0 m1).
-intros X m. 
-induction m.
-intros. unfold StFJoin0. simpl.
-induction m1 using StepF_ind.
-  assumption.
- revert IHm1_1. revert IHm1_2. unfold StFJoin0. simpl. intros.
- assert (H1:(constStepF (X:=StepFS X) x == m1_1)).
-  pose (s:=SplitL_resp_Xeq (constStepF (X:=StepFS X) x) 
-   (glue o m1_1 m1_2) o H). rewrite SplitLGlue  in s. rewrite s. auto with *.
- assert (H2:(constStepF (X:=StepFS X) x == m1_2)).
-  pose (s1:=SplitR_resp_Xeq (constStepF (X:=StepFS X) x) 
-   (glue o m1_1 m1_2) o H). rewrite SplitRGlue  in s1. rewrite s1. auto with *.
- symmetry. apply glue_StepF_eq.
- rewrite (IHm1_1 H1). reflexivity.
- rewrite (IHm1_2 H2). reflexivity.
-intros.
- pose (s:=SplitL_resp_Xeq 
-   (glue o m1 m2) m0 o H). rewrite SplitLGlue in s.
- unfold StFJoin0. simpl.
- change (glue o
-  (SplitL
-     (StFJoin0 m1) o)
-  (SplitR
-     (StFJoin0 m2) o) ==
-   (StFJoin0 m0)).
-rewrite (IHm1 _ s).
- pose (t:=SplitR_resp_Xeq 
-   (glue o m1 m2) m0 o H). rewrite SplitRGlue in t.
-rewrite (IHm2 _ t). 
-clear s t.
-clear H IHm1 IHm2.
-clear m1 m2.
-revert o.
-assert (forall m:StepFS (StepFS X), forall o : OpenUnit,
-glue o (SplitL (StFJoin0 (SplitL m o)) o)
-  (SplitR (StFJoin0 (SplitR m o)) o) == StFJoin0 m).
 
-
-
-induction m0.
-unfold StFJoin0. simpl.
-apply glueSplit.
-clear o.
-unfold StFJoin0. simpl.
-
-
-
-
-
-
-
- m1.
-induction m using StepF_ind. induction m1 using StepF_ind.
-  intros. assumption.
- intro. revert IHm1_1. revert IHm1_2. unfold StFJoin0. simpl. intros.
- assert (H1:(constStepF (X:=StepFS X) x == m1_1)).
-  pose (s:=SplitL_resp_Xeq (constStepF (X:=StepFS X) x) 
-   (glue o m1_1 m1_2) o H). rewrite SplitLGlue  in s. rewrite s. auto with *.
- assert (H2:(constStepF (X:=StepFS X) x == m1_2)).
-  pose (s1:=SplitR_resp_Xeq (constStepF (X:=StepFS X) x) 
-   (glue o m1_1 m1_2) o H). rewrite SplitRGlue  in s1. rewrite s1. auto with *.
- symmetry. apply glue_StepF_eq.
- rewrite (IHm1_1 H1). reflexivity.
- rewrite (IHm1_2 H2). reflexivity.
-intro.
-unfold StFJoin0.
-simpl. change (glue o
-  (SplitL
-     (StFJoin0 m2) o)
-  (SplitR
-     (StFJoin0 m3) o) ==
-   (StFJoin0 m1)).
-Admitted.
-
-
+(*
 Definition StFJoin (X:Setoid):(StepFS (StepFS X)) --> (StepFS X).
 intros X.
 exists (@StFJoin0 X).
 intros. apply (StFJoin_wd x1 x2). assumption.
 Defined.
+*)
 
+(*
 Definition StFBind (X Y:Setoid)(m:StepFS X)(f:X-->StepFS Y):(StepFS Y):=
 StFJoin0 (f^@>m).
 (* Map should have type --> *)
+*)
 
 (* 
 Definition Map' (X Y : Setoid):
@@ -167,138 +164,76 @@ Problem: Map has the wrong type *)
 (** Monad laws *)
 Variable X Y:Setoid.
 
-Lemma ReturnBind(x:X)(f:X-->StepFS Y): (StFBind (StFReturn X x) f)== (f x).
+Lemma ReturnBind(x:X)(f:X-->StepFS Y): (StFBind X Y (StFReturn X x) f)== (f x).
 simpl; auto with *.
 Qed.
 
-Definition Bind_compose(Z:Setoid)(f:X-->StepFS Y)(g:Y-->StepFS Z):X-->StepFS Z.
-intros.
-exists (fun x => (StFBind (f x) g)).
-abstract (intros; simpl; unfold StFBind;
-apply (StFJoin_wd );
-apply Map_resp_StepF_eq;[
-intros; apply g; assumption|
-apply f; assumption]).
-Defined.
+Definition Bind_compose(Z:Setoid)(f:X-->StepFS Y)(g:Y-->StepFS Z):=
+(compose ((flip (StFBind Y Z)) g) f).
 
-(*
-Lemma BindBind(Z:Setoid)(f:X-->StepFS Y)(g:Y-->StepFS Z)(m:StepF X): 
-(StFBind (StFBind m f) g) == (StFBind m (Bind_compose f g)).
-intros.
-unfold Bind_compose.
-unfold StFBind.
-unfold StFJoin0.
-induction m. simpl; auto with *.
+Lemma BindBind(Z:Setoid)(m:StepF X)(f:X-->StepFS Y)(g:Y-->StepFS Z):
+(StFBind Y Z (StFBind X Y m f) g) == (StFBind X Z m (Bind_compose f g)).
+intros Z m.
+induction m. simpl. unfold compose0. simpl; auto with *.
 simpl.
-rewrite IHm1. rewrite IHm2.
-auto with *.
+intros. apply glue_resp_StepF_eq.
+ clear IHm2 m2. simpl in IHm1.
+ rewrite (IHm1 (compose1 (SplitLS Y o) f) (compose1 (SplitLS Z o) g)).
+ clear IHm1. assert (extEq (StepFS Z)
+ (Bind_compose (compose1 (SplitLS Y o) f) (compose1 (SplitLS Z o) g)) 
+(compose1 (SplitLS Z o) (Bind_compose f g))).
+  intro. simpl. unfold compose0. pose (glueSplit (f x) o).
+  change (StFBind Y Z (SplitLS Y o (f x)) (compose (SplitLS Z o) g) ==
+  SplitLS Z o (StFBind Y Z (f x) g)).
+  assert (extEq (StepFS Z) (StFBind Y Z (f x))
+  (StFBind Y Z (glue o (SplitL (f x) o) (SplitR (f x) o)))).
+     symmetry in s. apply (Morphism_prf (StFBind Y Z) _ _ s).
+  clear s. 
+   (* SplitLS Z o should be a morphism
+     so that we can rewrite (H g) *)
+  rewrite (Morphism_prf (SplitLS Z o) _ _ (H g)).
+  simpl. unfold SplitLS0. rewrite SplitLGlue; auto with *.
+ apply (Morphism_prf (StFBind X Z m1) _ _ H).
+
+ clear IHm1 m1. simpl in IHm2.
+ rewrite (IHm2 (compose1 (SplitRS Y o) f) (compose1 (SplitRS Z o) g)).
+ clear IHm2. assert (extEq (StepFS Z)
+ (Bind_compose (compose1 (SplitRS Y o) f) (compose1 (SplitRS Z o) g)) 
+(compose1 (SplitRS Z o) (Bind_compose f g))).
+  intro. simpl. unfold compose0. pose (s:=glueSplit (f x) o).
+  change (StFBind Y Z (SplitRS Y o (f x)) (compose (SplitRS Z o) g) ==
+  SplitRS Z o (StFBind Y Z (f x) g)).
+  assert (H:extEq (StepFS Z) (StFBind Y Z (f x))
+  (StFBind Y Z (glue o (SplitL (f x) o) (SplitR (f x) o)))).
+     symmetry in s. apply (Morphism_prf (StFBind Y Z) _ _ s). clear s.
+  rewrite (Morphism_prf (SplitRS Z o) _ _ (H g)).
+  simpl. unfold SplitRS0. rewrite SplitRGlue; auto with *.
+ apply (Morphism_prf (StFBind X Z m2) _ _ H).
 Qed.
-*)
 
-Definition SSplitL(S:StepFS X):(StepFS (StepFS X)).
-intro f.
-apply (@StepFfold X (StepFS (StepFS X))).
-3:exact f. clear f.
-intro x.
-exact (StFReturn (StepFS X) (StFReturn X x)).
-intros o g h.
-exact (Map (fun x=> SplitL x o) g).
-Defined.
-
-Definition SSplitR(S:StepFS X):(StepFS (StepFS X)).
-intro f.
-apply (@StepFfold X (StepFS (StepFS X))).
-3:exact f. clear f.
-intro x.
-exact (StFReturn (StepFS X) (StFReturn X x)).
-intros o g h.
-exact (Map (fun x=> SplitR x o) h).
-Defined.
-
-Definition Diag(S:StepFS (StepFS X)):(StepFS (StepFS X)).
-fix 1.
-intro m.
-case m.
-exact (StFReturn (StepFS X)).
-intros o f g.
-pose (Map (fun x=> SplitL x o) f).
-pose (Map (fun x=> SplitR x o) g).
-apply (Map2 (glue o) s s0).
-Defined.
-
-(*
-Lemma JoinDiag:forall m, (StFJoin X m)==(StFJoin X (Diag m)).
-intros.
-induction m.
-reflexivity.
-pose (s := Map (fun x : StepF X => SplitL x o) m1).
-pose (s0 := Map (fun x : StepF X => SplitR x o) m2). 
-change ((@glue X o (SplitL (StFJoin X m1) o) (SplitR (StFJoin X m2) o))
-== StFJoin X (Diag (StepFunction.glue o m1 m2))).
-change (glue o (SplitL (StFJoin X m1) o) (SplitR (StFJoin X m2) o) 
-== StFJoin X (Map2 (glue o) s s0)).
-simpl.
-rewrite IHm1. rewrite IHm2.
-clear IHm1 IHm2.
-
-
-
-
-
-StFJoin0
-  (Map2 (glue o) (Map (fun x : StepF X => SplitL x o) m1)
-     (Map (fun x : StepF X => SplitR x o) m2))).
-
-
-unfold StFJoin0.
-simpl.
-*)
-
-
-
-
-
-Lemma BindReturn(m:StepF X): (StFBind m (StFReturn X)) == m.
+Lemma BindReturn(m:StepF X): (StFBind X X m (StFReturn X)) == m.
 intro m.
 unfold StFBind.
 induction m using StepF_ind.
-unfold SSplitL. simpl. auto with *.
-rewrite MapGlue.
-unfold SSplitL. simpl.
-rewrite IHm1. rewrite IHm2.
+ simpl. auto with *.
+simpl. 
+unfold StFBind00.
+simpl. apply glue_resp_StepF_eq.
+ clear IHm2 m2. simpl in IHm1. 
+ assert (extEq (StepFS X) (StFReturn X) 
+ (compose1 (SplitLS X o) (StFReturn X))).
+  intro. simpl. auto with *.
+ pose (s:=Morphism_prf (StFBind1 X m1) (StFReturn X)
+  (compose1 (SplitLS X o) (StFReturn X)) H).
+ rewrite s in IHm1. clear s H.
+ assumption.
 
-
-map ret (glue o (const x1) (const x2)) is
-
-glue o (const (ret x1)) (const (ret x2))
-
-now if we apply gjoin to it we get.
-
-glue o (const x1) (const x2)
-
-which is what we started with.
-
-
-intro m.
-unfold StFBind.
-assert ((StFReturn X ^@> m) == (SSplitL m)).
-induction m using StepF_ind.
-unfold SSplitL. simpl. auto with *.
-rewrite MapGlue.
-unfold SSplitL. simpl.
-rewrite IHm1. rewrite IHm2.
-
-
-
-induction m using StepF_ind. simpl;auto with *.
-
-rewrite MapGlue.
-assert ((StFReturn X ^@> m) == 
-unfold StFJoin0.
-simpl.
-apply glue_resp_StepF_eq. 
- clear IHm2 m2. rewrite  IHm1.
-(* SplitL m1 o == m1 ??*)
-rewrite GlueSplit.
-apply IHm1. apply IHm2.
+clear IHm1 m1. simpl in IHm2. 
+assert (extEq (StepFS X) (StFReturn X) 
+(compose1 (SplitRS X o) (StFReturn X))).
+intro; simpl; auto with *.
+pose (s:=Morphism_prf (StFBind1 X m2) (StFReturn X)
+(compose1 (SplitRS X o) (StFReturn X)) H).
+rewrite s in IHm2. clear s H.
+assumption.
 Qed.
