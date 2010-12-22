f87fd6c600f27b21c36d86f5278ecb42279eb59f
Eelis van der Weegen <eelis@eelis.net>
2010-12-22 14:44:41 +0100

    Add missing elementary operators for pairs.

--- stdlib_omissions/Pair.v
+++ stdlib_omissions/Pair.v
@@ -16,3 +16,10 @@ Definition first {A B C} (f: A → B) (p: A * C): B * C := (f (fst p), snd p).
 
 Lemma map_fst_map_first {A B C} (l: list (A * B)) (f: A -> C): map (@fst _ _) (map (first f) l) = map f (map (@fst _ _) l).
 Proof. induction l; simpl; congruence. Qed.
+
+Definition curry {A B C} (f: A * B → C) (a: A) (b: B): C := f (a, b).
+Definition uncurry {A B C} (f: A → B → C) (p: A * B): C := f (fst p) (snd p).
+
+Definition map_pair {X Y A B} (f: X → Y) (g: A → B) (xa: X * A): Y * B := (f (fst xa), g (snd xa)).
+
+Definition diagonal {X} (x: X): X * X := (x, x).
