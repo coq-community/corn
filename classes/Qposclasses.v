d9951cf78c44ea2fd49554b71b8913fa99a6676b
Robbert Krebbers <mail@robbertkrebbers.nl>
2011-02-11 15:24:33 +0100

    Canonical name for pow on Qpos.

--- classes/Qposclasses.v
+++ classes/Qposclasses.v
@@ -1,6 +1,9 @@
 Require Import Qpossec.
-Require Import canonical_names.
+Require Import canonical_names additional_operations.
 
+Instance: Equiv Qpos := QposEq.
 Instance: RingOne Qpos := Qpos_one.
 Instance: RingPlus Qpos := Qpos_plus.
 Instance: RingMult Qpos := Qpos_mult.
+
+Instance: Pow Qpos Z := Qpos_power.
