b90cb5032176d1b4eca26db9edc6eaef773177a7
Robbert Krebbers <mail@robbertkrebbers.nl>
2011-03-11 02:51:05 +0100

    Prove that the dyadics are an approximate rationals.

--- reals/faster/ARDyadics.v
+++ reals/faster/ARDyadics.v
@@ -1,84 +1,172 @@
 Require Import
-  Program QArith ZArith BigZ Qpossec
+  Program QArith ZArith BigZ Qpossec BigZeuclid
   MetricMorphisms Qmetric Qdlog2
   ApproximateRationals ARArith
   interfaces.integers interfaces.rationals
-  stdlib_rationals stdlib_binary_integers fast_integers
-  dyadics nonneg_integers_naturals.
+  theory.int_pow theory.nat_pow
+  stdlib_rationals stdlib_binary_integers fast_integers dyadics.
+
+Add Field Q : (fields.stdlib_field_theory Q).
 
 Notation fastD := (Dyadic fastZ).
+Instance fastZtoQ : Coerce fastZ Q_as_MetricSpace := inject_Z ∘ BigZ.to_Z.
+Instance ZtofastD : Coerce Z fastD := dy_inject ∘ BigZ.of_Z.
+Instance NtofastZ : Coerce N fastZ := BigZ.of_Z ∘ Z_of_N.
+Instance fastDtoQ : Coerce fastD Q_as_MetricSpace := DtoQ fastZtoQ.
 
-Definition fastZtoQ : fastZ → Q_as_MetricSpace := inject_Z ∘ BigZ.to_Z.
-Instance fastDtoQ: Coerce fastD Q_as_MetricSpace := DtoQ fastZtoQ.
-
-Program Instance fastD_compress : AppCompress fastD := λ x k,
-  let s := (BigZ.of_Z k - 1) - expo x
-  in if decide_rel (≤) 0 s
-  then (mant x ≫ exist _ s _) $ (BigZ.of_Z k - 1)
-  else x.
-
-Instance: Proper ((=) ==> (=) ==> (=)) fastD_compress.
+Lemma fastDtoQ_correct x : fastDtoQ x = 'mant x * 2 ^ ('expo x : Z).
 Proof.
-  assert (∀ x1 x2 k, x1 = x2 → expo x1 ≤ expo x2 → fastD_compress x1 k = fastD_compress x2 k).
-   intros [x1m x1e] [x2m x2e] k Ex Ee.
-   apply rings.flip_nonneg_minus in Ee.
-   pose proof (proj2 (dy_eq_dec_aux (x1m $ x1e) (x2m $ x2e) Ee) Ex) as Ex'.
-   unfold equiv, dy_equiv, DtoQ_slow, fastD_compress.
-   case (decide_rel _); case (decide_rel _); intros E1 E2; simpl in *.
-      apply sg_mor; [| reflexivity].
-      apply sm_proper.
-      unfold shiftr, BigZ_Integers.ShiftR_instance_0. simpl.
-      rewrite Ex'. unfold shiftl, BigZ_Integers.ZType_shiftl. simpl.
-      rewrite BigZ.shiftr_shiftl_r; [|easy].
-      admit.
-     admit.
-    rewrite Ex'.
-    rewrite ZtoQ_shift.
-    admit.
-   easy.
-  intros x1 x2 Ex k1 k2 Ek.
-  unfold equiv, Z_equiv in Ek. rewrite Ek.
-  destruct (total_order (expo x1) (expo x2)).
-   auto.
-  symmetry. symmetry in Ex. auto.
+  unfold fastDtoQ.
+  rewrite (DtoQ_correct _ _ (reflexivity x)).
+  unfold DtoQ_slow.
+  now rewrite (preserves_int_pow_exp (f:=coerce : bigZ → Z)).
 Qed.
 
-Lemma fastD_compress_correct (x : fastD) (k : Z) : Qball (2 ^ k) ('app_compress x k) ('x).
-Proof.
-  unfold app_compress, fastD_compress.
-  case (decide_rel _); intros E; simpl in *.
-   admit.
-  apply ball_refl.
-Qed.
-
+(* 
+  We use BigZ.div and BigZ.shiftl because we don't have any theory on euclid and shiftr
+  in math-classes yet. Moreover, BigZ.shiftl behaves as shiftr on its negative domain,
+  which is quite convenient here.
+*)
 Program Instance fastD_div : AppDiv fastD := λ x y k,
-  let s := -(BigZ.of_Z k - 1) + expo x - expo y
-  in if decide_rel (≤) 0 s 
-  then (mant x ≪ exist _ s _) `div` (mant y) $ (BigZ.of_Z k - 1)
-  else (mant x ≫ exist _ (-s) _) `div` (mant y) $ (BigZ.of_Z k - 1).
-Next Obligation.
-  apply rings.flip_nonpos_opp.
-  now apply orders.precedes_flip. 
+  BigZ.div (BigZ.shiftl (mant x) (-('k - 1) + expo x - expo y)) (mant y) $ ('k - 1).
+
+Lemma Qdiv_bounded_Zdiv (x y : Z) :
+  'Zdiv x y ≤ ('x / 'y : Q) < 'Zdiv x y + 1.
+Proof.
+  rewrite Qround.Zdiv_Qdiv.
+  split.
+   now apply Qround.Qfloor_le.
+  rewrite <-(rings.preserves_1 (f:=coerce : Z → Q)).
+  rewrite <-rings.preserves_plus.
+  apply stdlib_rationals.Qlt_coincides.
+  now apply Qround.Qlt_floor.
 Qed.
 
-Instance: Proper ((=) ==> (=) ==> (=) ==> (=)) fastD_div.
+Lemma Qpow_bounded_Zshiftl (x n : Z) : 
+  'Zshiftl x n ≤ ('x * 2 ^ n : Q) < 'Zshiftl x n + 1.
 Proof.
-  intros [x1m x1e] [x2m x2e] Ex [y1m y1e] [y2m y2e] Ey k1 k2 Ek.
-  unfold equiv, dy_equiv, DtoQ_slow in Ex, Ey |- *.
-  unfold fastD_div.
-  case (decide_rel _); case (decide_rel _); intros E1 E2; simpl in *.
-Admitted.
+  destruct (total_order 0 n) as [En | En].
+   rewrite Z.shiftl_mul_pow2 by trivial.
+   rewrite inject_Z_mult.
+   rewrite Qpower.Zpower_Qpower by trivial.
+   split; try reflexivity.
+   apply semirings.pos_plus_scompat_r; try reflexivity.
+   apply semirings.sprecedes_0_1.
+  rewrite Z.shiftl_div_pow2 by trivial.
+  assert (('x * 2 ^ n : Q) = 'x / 'Zpower 2 (-n)).
+   rewrite Qpower.Zpower_Qpower.
+    rewrite <-Qpower.Qpower_opp, Zopp_involutive.
+    reflexivity.
+   now apply rings.flip_nonpos_opp in En.
+  split.
+   transitivity ('x / 'Zpower 2 (-n) : Q).
+    now apply Qdiv_bounded_Zdiv.
+   apply orders.equiv_precedes. now symmetry.
+  apply orders.sprecedes_trans_r with ('x / 'Zpower 2 (-n) : Q).
+   now apply orders.equiv_precedes.
+  now apply Qdiv_bounded_Zdiv.
+Qed.
 
 Lemma fastD_div_correct (x y : fastD) (k : Z) : Qball (2 ^ k) ('app_div x y k) ('x / 'y).
 Proof.
-Admitted.
+  assert (∀ xm xe ym ye : Z, 
+      ('xm * 2 ^ xe : Q) / ('ym * 2 ^ ye : Q) = ('xm * 2 ^ (-(k - 1) + xe - ye)) / 'ym * 2 ^ (k - 1)) as E1.
+   intros.
+   rewrite 2!int_pow_exp_plus by solve_propholds.
+   rewrite fields.dec_mult_inv_distr.
+   rewrite 2!int_pow_opp.
+   transitivity ('xm / 'ym * 2 ^ xe / 2 ^ ye * (2 ^ (k - 1) / 2 ^ (k - 1))); [| ring].
+   rewrite fields.dec_mult_inverse by apply _. ring.
+  assert (∀ xm xe ym ye : Z, 
+      'Zdiv (Zshiftl xm (-(k - 1) + xe - ye)) ym * 2 ^ (k - 1) - 2 ^ k  ≤ ('xm * 2 ^ xe) / ('ym * 2 ^ ye)) as Pleft.
+   clear x y.
+   assert (∀ z : Q, z * 2 ^ (k - 1) - 2 ^ k = ((z - 1) - 1) * 2 ^ (k - 1)) as E2.
+    intros.
+    setoid_replace k with ((k - 1) + 1) using relation (@equiv Z _) at 2 by ring.
+    rewrite (int_pow_exp_plus (k - 1)) by solve_propholds.
+    ring_simplify. apply sg_mor; [reflexivity | now rewrite commutativity].
+   intros. rewrite E1, E2.
+   apply (order_preserving (.* _)).
+   apply rings.flip_minus_l. apply semirings.nonneg_plus_compat_r; [| easy].
+   transitivity ('Zshiftl xm (-(k - 1) + xe - ye) / 'ym - 1 : Q).
+    apply (order_preserving (+ -1)). now apply Qdiv_bounded_Zdiv.
+   destruct (orders.precedes_or_sprecedes 0 ym) as [E | E].
+    apply rings.flip_minus_l. apply semirings.nonneg_plus_compat_r; [| easy].
+    apply (maps.order_preserving_flip_ge_0 (.*.) (/ 'ym : Q)).
+     apply fields.nonneg_dec_mult_inv_compat.
+     now apply semirings.preserves_nonneg.
+    now apply Qpow_bounded_Zshiftl.
+   transitivity (('Zshiftl xm (-(k - 1) + xe - ye) + 1) / 'ym : Q).
+    rewrite rings.plus_mul_distr_r.
+    apply semirings.plus_compat; [reflexivity |].
+    rewrite rings.mult_1_l.
+    apply rings.flip_opp.
+    rewrite rings.opp_involutive, fields.dec_mult_inv_opp.
+    apply fields.flip_dec_mult_inv_l; [solve_propholds |].
+    rewrite <-rings.preserves_opp.
+    apply semirings.preserves_ge1.
+    apply rings.flip_opp.
+    rewrite rings.opp_involutive.
+    now apply integers.precedes_sprecedes.
+   apply rings.flip_nonpos_mult_r.
+    apply fields.nonpos_dec_mult_inv_compat.
+    apply semirings.preserves_nonpos.
+    now apply orders.sprecedes_weaken. 
+   now apply Qpow_bounded_Zshiftl.
+  assert (∀ xm xe ym ye : Z, 
+      ('xm * 2 ^ xe) / ('ym * 2 ^ ye) ≤ '(Zdiv (Zshiftl xm (-(k - 1) + xe - ye)) ym) * 2 ^ (k - 1) + 2 ^ k) as Pright.
+   clear x y.
+   assert (∀ z : Q, z * 2 ^ (k - 1) + 2 ^ k = ((z + 1) + 1) * 2 ^ (k - 1)) as E2.
+    intros.
+    setoid_replace k with ((k - 1) + 1) using relation (@equiv Z _) at 2 by ring.
+    rewrite (int_pow_exp_plus (k - 1)) by solve_propholds.
+    ring_simplify. apply sg_mor; [reflexivity| apply commutativity].
+   intros. rewrite E1, E2.
+   apply (order_preserving (.* _)).
+   transitivity ('Zshiftl xm (-(k - 1) + xe - ye) / 'ym + 1 : Q).
+    2: now apply (order_preserving (+1)), Qdiv_bounded_Zdiv.
+   destruct (orders.precedes_or_sprecedes ym 0) as [E3 | E3].
+    apply semirings.nonneg_plus_compat_r; [|easy].
+    apply rings.flip_nonpos_mult_r.
+     apply fields.nonpos_dec_mult_inv_compat.
+     now apply semirings.preserves_nonpos.
+    now apply Qpow_bounded_Zshiftl.
+   transitivity (('Zshiftl xm (-(k - 1) + xe - ye) + 1) / ' ym : Q).
+    apply (maps.order_preserving_flip_ge_0 (.*.) (/ 'ym : Q)).
+     apply fields.nonneg_dec_mult_inv_compat.
+     apply semirings.preserves_nonneg.
+     now apply orders.sprecedes_weaken. 
+    now apply Qpow_bounded_Zshiftl.
+   rewrite rings.plus_mul_distr_r.
+   apply semirings.plus_compat; [reflexivity |].
+   rewrite rings.mult_1_l.
+   apply fields.flip_dec_mult_inv_l; [solve_propholds |].
+   apply semirings.preserves_ge1.
+   now apply integers.precedes_sprecedes_alt in E3.
+  unfold coerce. rewrite 3!fastDtoQ_correct.
+  destruct x as [xm xe], y as [ym ye].
+  unfold fastZtoQ, coerce, "∘". simpl. unfold coerce. BigZ.zify.
+  apply in_Qball. split. apply Pleft. apply Pright.
+Qed.
 
 Instance QtofastD: AppInverse fastDtoQ := λ x ε, 
-  app_div ('BigZ.of_Z (Qnum x)) ('BigZ.of_Z (Qden x)) (Qdlog2 ε).
+  app_div ('Qnum x) ('(Qden x : Z)) (Qdlog2 ε).
 
-Instance: Proper ((=) ==> (=) ==> (=)) QtofastD.
+Instance fastD_compress : AppCompress fastD := λ x k,
+  BigZ.shiftl (mant x) (-('k - 1) + expo x) $ ('k - 1).
+
+Lemma fastD_compress_correct (x y : fastD) (k : Z) : Qball (2 ^ k) ('app_compress x k) ('x).
 Proof.
-Admitted.
+  setoid_replace (app_compress x k) with (app_div x 1 k).
+   setoid_replace ('x) with ('x / '1).
+    now apply fastD_div_correct.
+   rewrite rings.preserves_1, fields.dec_mult_inv_1.
+   now rewrite rings.mult_1_r.
+  unfold app_div, fastD_div.
+  simpl. rewrite BigZ.div_1_r.
+  setoid_replace (-('k - 1) + expo x - 0) with (-('k - 1) + expo x); [reflexivity |].
+  now rewrite rings.opp_0, rings.plus_0_r.
+Qed.
 
 Instance: DenseEmbedding fastDtoQ.
 Proof.
@@ -92,7 +180,7 @@ Proof.
   apply fastD_div_correct.
 Qed.
 
-Instance fastD_Zshiftl: ShiftL fastD Z := λ x n, x ≪ BigZ.of_Z n.
+Instance fastD_Zshiftl: ShiftL fastD Z := λ x n, x ≪ 'n.
 
 Instance: Proper ((=) ==> (=) ==> (=)) fastD_Zshiftl.
 Proof. unfold fastD_Zshiftl. solve_proper. Qed.
@@ -104,18 +192,39 @@ Proof.
   intros x n. rewrite rings.preserves_plus. now apply shiftl_S.
 Qed.
 
-Instance fastD_Npow: Pow fastD N := λ x n, (mant x) ^ n $ BigZ.of_Z (Z_of_N n) * expo x.
-Instance: NatPowSpec fastD N fastD_NPow.
-Proof.
-Admitted.
+(* 
+  This function is more or less a copy of dy_pow, but uses [N] instead of [BigZ⁺] for the exponent. 
+  An alternative definition would have been fastD_Npow x n = dy_pow x (N_to_BigZ_NonNeg n).
+  However, then the exponent would be translated from [N] into [BigZ] and back again, due to the 
+  definition of [BigZ.pow]. 
+*) 
+Instance fastD_Npow: Pow fastD N := λ x n, (mant x) ^ n $ 'n * expo x.
 
-Instance ZtofastD: Coerce Z fastD := dy_inject ∘ BigZ.of_Z.
+Instance: NatPowSpec fastD N fastD_Npow.
+Proof.
+  split; unfold "^", fastD_Npow, equiv, dy_equiv, DtoQ_slow.
+    intros [xm xe] [ym ye] E1 e1 e2 E2. simpl in *.
+    rewrite E2. clear e1 E2.
+    rewrite 2!(preserves_nat_pow (f:=integers_to_ring fastZ Q)).
+    rewrite 2!(commutativity ('e2)).
+    rewrite 2!int_pow_exp_mult.
+    rewrite 2!(int_pow_nat_pow (f:=coerce : N → fastZ)).
+    rewrite <-2!nat_pow_base_mult.
+    now rewrite E1.
+   intros [xm xe]. simpl.
+   rewrite rings.preserves_0, left_absorb.
+   now rewrite nat_pow_0.
+  intros [xm xe] n. simpl.
+  rewrite nat_pow_S.
+  rewrite rings.preserves_plus, rings.preserves_1.
+  now rewrite distribute_r, left_identity.
+Qed.
 
 Instance: AppRationals fastD.
 Proof.
-  split; try apply _.
-   apply fastD_div_correct.
-  apply fastD_compress_correct.
+  split; try apply _; intros.
+   now apply fastD_div_correct.
+  now apply fastD_compress_correct.
 Qed.
 
-Definition fastAR := AR (AQ:=fastD).
+Notation fastAR := (AR (AQ:=fastD)).
