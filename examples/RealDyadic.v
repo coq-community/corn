15d847f73b5f7ac12e0e462ab11cc630f21818da
Robbert Krebbers <mail@robbertkrebbers.nl>
2011-03-24 21:29:56 +0100

    Various examples from the "Many Digits friendly competition".

--- examples/RealDyadic.v
+++ examples/RealDyadic.v
@@ -1,39 +1,59 @@
-Require Import BigZ ARDyadics CRArith ARexp ARpi ARarctan ARroot ARsign.
+Require Import BigZ CRArith ARDyadics ARtrans ARsign.
 
 Definition answer (n:positive) (r : fastAR) : bigZ :=
  let m := (iter_pos n _ (Pmult 10) 1%positive) in 
  let (a, b) := (approximate r (1#m)%Qpos : fastD) * 'Zpos m in 
  BigZ.shiftl a b.
 
-Let ARtest1 : fastAR := 2 * ARexp (ARexp (AQexp 1)).
-Time Eval vm_compute in (answer 150 ARtest1).
+(* Many of the following expressions are taken from the "Many Digits friendly competition" problem set *)
 
-Let ARtest2 : fastAR := '(4 : Z) + AQexp ('(10 : Z)).
-Time Eval vm_compute in (answer 1250 ARtest2).
+Definition P01 : fastAR := ARsin (ARsin (AQsin 1)).
+Time Eval vm_compute in (answer 150 P01).
 
-Let ARtest3 : fastAR := ARpi.
-Time Eval vm_compute in (answer 750 ARtest3).
+Definition P02 : fastAR := ARsqrt (ARcompress ARpi).
+Time Eval vm_compute in (answer 250 P02).
 
-Let ARtest4 : fastAR := ARexp ARpi - ARpi.
+Definition P03 : fastAR := ARsin (AQexp 1).
+Time Eval vm_compute in (answer 200 P03).
+
+Definition P04 : fastAR := ARexp (ARcompress (ARpi * AQsqrt ('163%Z))).
+Time Eval vm_compute in (answer 175 P04).
+
+Definition P05 : fastAR := ARexp (ARexp (AQexp 1)).
+Time Eval vm_compute in (answer 150 P05).
+
+Definition P07 : fastAR := AQexp ('1000%Z).
+Time Eval vm_compute in (answer 500 P07).
+
+Definition P08 : fastAR := AQcos ('(10^50)%Z).
+Time Eval vm_compute in (answer 350 P08).
+
+Definition C02_prf : (ARpi : fastAR) >< (0 : fastAR).
+Proof. right. AR_solve_lt (-8)%Z. Defined.
+
+Definition C02 : fastAR := ARsqrt (AQexp 1 * ARinv ARpi C02_prf).
+Time Eval vm_compute in (answer 150 C02).
+
+Definition C03 : fastAR := ARsin (ARcompress (ARpower_positive 3 (AQexp 1 + 1))).
+Time Eval vm_compute in (answer 200 C03).
+
+Definition C04 : fastAR := ARexp (ARcompress (ARpi * AQsqrt ('2011%Z))).
+Time Eval vm_compute in (answer 100 C04).
+
+Definition C05 : fastAR := ARexp (ARexp (ARsqrt (AQexp 1))).
+Time Eval vm_compute in (answer 150 C05).
+
+Definition ARtest1 : fastAR := ARpi.
+Time Eval vm_compute in (answer 750 ARtest1).
+
+Definition ARtest2 : fastAR := ARarctan (ARcompress ARpi).
+Time Eval vm_compute in (answer 35 ARtest2).
+
+Definition ARtest3 : fastAR := ARsqrt 2.
+Time Eval vm_compute in (answer 1000 ARtest3).
+
+Definition ARtest4 : fastAR := ARsin ARpi.
 Time Eval vm_compute in (answer 200 ARtest4).
-
-Let ARtest5 : fastAR := ARarctan (ARcompress ARpi).
-Time Eval vm_compute in (answer 35 ARtest5).
-
-Let ARtest6 : fastAR := ARsqrt 2.
-Time Eval vm_compute in (answer 1000 ARtest6).
-
-Let ARtest7 : fastAR := ARsqrt (ARexp 1).
-Time Eval vm_compute in (answer 500 ARtest7).
-
-Let ARtest8 : fastAR := ARexp (ARsqrt 2).
-Time Eval vm_compute in (answer 200 ARtest8).
-
-Local Obligation Tactic := idtac.
-Program Let ARtest9 := ARinv (ARsqrt (ARpi : fastAR)) _.
-Next Obligation. right. AR_solve_lt (-8)%Z. Defined.
-
-Time Eval vm_compute in (answer 300 ARtest9).
 
 Example xkcd217A : ARtest4 ⋖ '20%Z.
 Proof. Time AR_solve_lt (-8)%Z. Defined.
