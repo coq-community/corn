e25595df54c2c95e98f7aa7582cfca32b747c01f
Jelle Herold <jelle@defekt.nl>
2011-10-14 20:11:09 +0200

    Replaced meet/join by their unicode notation ⊔ / ⊓

--- classes/Qclasses.v
+++ classes/Qclasses.v
@@ -5,14 +5,14 @@ Require Import
 Require Export 
   stdlib_rationals.
 
-Lemma Qmin_coincides x y : Qmin x y = meet x y.
+Lemma Qmin_coincides x y : Qmin x y = x ⊓ y.
 Proof.
   destruct (total (≤) x y).
    rewrite lattices.meet_l by easy. now apply Qle_min_l.
   rewrite lattices.meet_r by easy. now apply Qle_min_r.
 Qed.
 
-Lemma Qmax_coincides x y : Qmax x y = join x y.
+Lemma Qmax_coincides x y : Qmax x y = x ⊔ y.
 Proof.
   destruct (total (≤) x y).
    rewrite lattices.join_r by easy. now apply Qle_max_r.
