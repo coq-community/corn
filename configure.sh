0da96ae5eea65b05c6bfea89c0df451cbeacca7a
Guillaume Claret <dev@clarus.me>
2014-09-25 18:22:07 +0200

    The configure generate Make using find

--- configure.sh
+++ configure.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
+cp -f Make.in Make
+find algebra complex coq_reals fta ftc logic metrics model raster reals tactics transc order metric2 stdlib_omissions util classes ode -name "*.v" |grep -v tactics/Opaque_algebra.v >>Make
 coq_makefile -f Make -o Makefile
