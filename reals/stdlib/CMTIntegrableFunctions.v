6b0b56bb997e878083d6a235c0c880c557b02c70
Ana Dalton <anakata@google.com>
2023-05-17 09:54:10 -0700

    'Deprecate mult_assoc for Nat.mul_assoc'

--- reals/stdlib/CMTIntegrableFunctions.v
+++ reals/stdlib/CMTIntegrableFunctions.v
@@ -409,7 +409,7 @@ Proof.
       unfold Nat.odd in H3. rewrite des in H3. inversion H3.
       apply cv. apply (Nat.le_trans N n). assumption.
       apply (Nat.le_trans n (n*2)). rewrite <- (Nat.mul_1_r n).
-      rewrite <- mult_assoc. apply Nat.mul_le_mono_nonneg_l.
+      rewrite <- Nat.mul_assoc. apply Nat.mul_le_mono_nonneg_l.
       apply Nat.le_0_l. apply le_S. apply Nat.le_refl.
       rewrite <- (Nat.add_0_l (n*2)). rewrite Nat.add_assoc.
       apply Nat.add_le_mono_r. auto.
