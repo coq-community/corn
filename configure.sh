5ea1a14bbc3200ebb3fe9138e339031e588ebe6b
Ana Dalton <anakata@google.com>
2023-05-30 11:17:36 -0700

    No need to check COQ version in configure.sh

--- configure.sh
+++ configure.sh
@@ -4,16 +4,8 @@
 
 cp -f Make.in Make
 
-COQ_VERSION_LINE=$(coqc --version | head -1)
-COQ_VERSION=`echo $COQ_VERSION_LINE | sed 's/The Coq Proof Assistant, version 8\.\([^ +]*\).*/\1/'`
 DIRECTORIES="algebra complex coq_reals fta ftc liouville logic metrics model raster reals tactics transc order metric2 stdlib_omissions util classes ode"
 
-# Include constructive measure theory on version 8.12 and after
-if [ $COQ_VERSION -gt 11 ] ;
-then
-    find $DIRECTORIES -name "*.v" >>Make
-else
-    find $DIRECTORIES -name "*.v" | grep -v "stdlib/" >>Make
-fi
+find $DIRECTORIES -name "*.v" >>Make
 
 ${COQBIN}coq_makefile -f Make -o Makefile
